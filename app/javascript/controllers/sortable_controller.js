f31413d4e92e621bedfdca2b671e82c2f55c2cda
Mayara Vass <mayara_mv93@outlook.com>
2023-06-08 20:03:33 -0300

    Tasks solved

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,54 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+
+export default class extends Controller {
+  static targets = ["container"]
+  static values = {}
+
+  connect() {
+    console.log('vai')
+    this.variable = this.element.querySelector(".card-task").dataset.sortableUrlValue
+    this.containerTargets.forEach((cont) => {
+      Sortable.create(cont, {
+        animation: 150,
+        group: "shared",
+        handle: ".handle",
+        filter: '.filtered',
+        onEnd: (event) => {
+          let url = event.originalEvent.dataTransfer.getData("application/drag-key")
+          const csrfToken = document.head.querySelector("[name='csrf-token']").content;
+
+          fetch(url, {
+            method:"PATCH",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'X-Requested-With': 'XMLHttpRequest',
+              'X-CSRF-Token': csrfToken
+            },
+            body: JSON.stringify({task: {step: event.to.dataset.filter}})
+          })
+        }
+      })
+    })
+  }
+
+  dragstart(event) {
+    event.dataTransfer.setData("application/drag-key", event.currentTarget.dataset.url)
+  }
+
+  dragover(event) {
+    event.preventDefault()
+  }
+
+  dragenter(event) {
+    event.preventDefault()
+  }
+
+  drop(event) {
+    event.preventDefault()
+  }
+  dragend(event) {
+    event.preventDefault()
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
     end
     unauthenticated :user do
       root 'pages#home'
-end
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tasks, except: :index
   resources :clients
   # Defines the root path route ("/")
   # root "articles#index"
+  patch '/tasks', to: 'tasks#index'
 end
